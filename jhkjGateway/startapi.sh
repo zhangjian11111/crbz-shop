fd1fa3cb67f1424ebedf5a60b840d81713889628
zhangj <1097951193@qq.com>
2023-08-06 04:07:15 +0800

    新错误

--- jhkjGateway/startapi.sh
+++ jhkjGateway/startapi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-java -Xmx256m -Xms256m -Xss256k -jar -Duser.timezone=GMT+08 /data/crbz/jhkjGateway-4.2.5.jar> logs/jhkjGateway.out  & \
+java -Dreactor.netty.pool.leasingStrategy=lifo -Xmx256m -Xms256m -Xss256k -jar -Duser.timezone=GMT+08 /data/crbz/jhkjGateway-4.2.5.jar> logs/jhkjGateway.out  & \
 # 死循环，保持docker前台运行
 while [[ true ]]; do
     sleep 1

bbd1644d1c5c0bab9b688edae12cc587700f4e3f
zhangj <1097951193@qq.com>
2023-08-16 16:55:31 +0800

    小程序管理员可以查看所有用户订单

--- buyer-api/src/main/java/cn/crbz/controller/order/CancelAfterVerificaionController.java
+++ buyer-api/src/main/java/cn/crbz/controller/order/CancelAfterVerificaionController.java
@@ -3,17 +3,17 @@ package cn.crbz.controller.order;
 import cn.crbz.common.aop.annotation.PreventDuplicateSubmissions;
 import cn.crbz.common.enums.ResultUtil;
 import cn.crbz.common.vo.ResultMessage;
+import cn.crbz.modules.order.order.entity.dto.OrderSearchParams;
+import cn.crbz.modules.order.order.entity.vo.OrderSimpleVO;
 import cn.crbz.modules.order.order.service.OrderService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 /**
@@ -44,6 +44,13 @@ public class CancelAfterVerificaionController {
     public ResultMessage<Object> mpTake(@PathVariable String orderSn, @PathVariable String verificationCode) {
         log.info("核销参数---注意参数检查是否有空格：：："+orderSn+"***"+verificationCode);
         return ResultUtil.data(orderService.mpTake(orderSn,verificationCode));
+    }
+
+
+    @ApiOperation(value = "查询所有订单列表")
+    @GetMapping(value = "/showAllOrders")
+    public ResultMessage<IPage<OrderSimpleVO>> queryAllOrder(OrderSearchParams orderSearchParams) {
+        return ResultUtil.data(orderService.queryByParams(orderSearchParams));
     }
 
 
